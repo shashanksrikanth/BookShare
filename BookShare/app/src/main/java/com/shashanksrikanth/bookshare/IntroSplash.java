83e32d66a3a2dfde854ff6a106b719ad62b0c8a6
shashanksrikanth1 <sshashank1999@gmail.com>
2020-11-29 14:31:24 -0600

    BOOK-2|Adding splash screen

---
+++ BookShare/app/src/main/java/com/shashanksrikanth/bookshare/IntroSplash.java
@@ -0,0 +1,25 @@
+package com.shashanksrikanth.bookshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class IntroSplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent startMainActivity = new Intent(IntroSplash.this, MainActivity.class);
+                startActivity(startMainActivity);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                finish();
+            }
+        }, 2000);
+    }
+}
