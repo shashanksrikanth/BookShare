43b0f8b83219503a75343a365ae872d6d9dc4118
shashanksrikanth1 <sshashank1999@gmail.com>
2020-11-29 14:01:09 -0600

    BOOK-2|Adding sign in and sign up activities

---
+++ BookShare/app/src/main/java/com/shashanksrikanth/bookshare/UserSignIn.java
@@ -0,0 +1,14 @@
+package com.shashanksrikanth.bookshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserSignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_sign_in);
+    }
+}
