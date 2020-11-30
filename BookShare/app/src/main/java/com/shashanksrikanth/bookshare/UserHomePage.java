59ec113202d3b07ce876499126f94a9fb885ecd5
shashanksrikanth1 <sshashank1999@gmail.com>
2020-11-30 17:34:52 -0600

    BOOK-5|Added drawer menu

--- BookShare/app/src/main/java/com/shashanksrikanth/bookshare/UserHomePage.java
+++ BookShare/app/src/main/java/com/shashanksrikanth/bookshare/UserHomePage.java
@@ -1,11 +1,20 @@
 package com.shashanksrikanth.bookshare;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.drawerlayout.widget.DrawerLayout;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -17,8 +26,13 @@ import com.google.firebase.database.ValueEventListener;
 public class UserHomePage extends AppCompatActivity {
 
     TextView userNameLabel;
+    DrawerLayout drawerLayout;
+    ListView drawerList;
+    ActionBarDrawerToggle drawerToggle;
+    String[] drawerItems;
     String userID;
     boolean goBackToPreviousActivity;
+    private static final String TAG = "UserHomePage";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +63,23 @@ public class UserHomePage extends AppCompatActivity {
 
             }
         });
+
+        // Set up drawer menu
+        drawerItems = new String[] {"Donor Page", "Receiver Page"};
+        drawerLayout = findViewById(R.id.drawer_layout);
+        drawerList = findViewById(R.id.left_drawer);
+        drawerList.setAdapter(new ArrayAdapter<>(this, R.layout.drawer_list_item, drawerItems));
+        drawerList.setOnItemClickListener(new ListView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectItem(position);
+            }
+        });
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
+        if(getSupportActionBar()!=null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
     }
 
     private void getName(DataSnapshot snapshot) {
@@ -63,5 +94,32 @@ public class UserHomePage extends AppCompatActivity {
     public void onBackPressed() {
         if(goBackToPreviousActivity) super.onBackPressed();
         else return;
+    }
+
+    private void selectItem(int position) {
+        // Dictates what to do when a drawer item is selected
+        String message = "You have selected " + drawerItems[position];
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        drawerLayout.closeDrawer(drawerList);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState(); // <== IMPORTANT
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(drawerToggle.onOptionsItemSelected(item)) {
+            Log.d(TAG, "onOptionsItemSelected: drawerToggle " + item);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
