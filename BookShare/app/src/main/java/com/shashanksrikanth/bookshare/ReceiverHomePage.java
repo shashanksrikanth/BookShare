0f7c4a15071b5190ea652a9e4d81559cadc8aa1f
shashanksrikanth1 <sshashank1999@gmail.com>
2020-11-30 22:00:02 -0600

    BOOK-10|Adding drawer menu to receiver home

--- BookShare/app/src/main/java/com/shashanksrikanth/bookshare/ReceiverHomePage.java
+++ BookShare/app/src/main/java/com/shashanksrikanth/bookshare/ReceiverHomePage.java
@@ -1,17 +1,82 @@
 package com.shashanksrikanth.bookshare;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class ReceiverHomePage extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ListView drawerList;
+    ActionBarDrawerToggle drawerToggle;
+    String[] drawerItems;
+    private static final String TAG = "ReceiverHomePage";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receiver_home_page);
 
-        Toast.makeText(this, "You are in ReceiverHomePage", Toast.LENGTH_LONG).show();
+        // Set up drawer menu
+        drawerItems = new String[] {"Home Page", "Donor Page"};
+        drawerLayout = findViewById(R.id.drawerLayoutReceiverHomePage);
+        drawerList = findViewById(R.id.leftDrawerReceiverHomePage);
+        drawerList.setAdapter(new ArrayAdapter<>(this, R.layout.drawer_list_item, drawerItems));
+        drawerList.setOnItemClickListener(new ListView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectItem(position);
+            }
+        });
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
+        if(getSupportActionBar()!=null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
+    }
+
+    private void selectItem(int position) {
+        // Dictates what to do when a drawer item is selected
+        if(drawerItems[position].equals("Home Page")) {
+            Intent intent = new Intent(this, UserHomePage.class);
+            startActivity(intent);
+        }
+        else {
+            Intent intent = new Intent(this, DonorHomePage.class);
+            startActivity(intent);
+        }
+        drawerLayout.closeDrawer(drawerList);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(drawerToggle.onOptionsItemSelected(item)) {
+            Log.d(TAG, "onOptionsItemSelected: drawerToggle " + item);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
