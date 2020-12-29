059338af2826f363ddd4bbd16db2ea12282d5d14
shashanksrikanth1 <sshashank1999@gmail.com>
2020-12-29 10:49:36 -0600

    BOOK-20|Adding network check

--- BookShare/app/src/main/java/com/shashanksrikanth/bookshare/MainActivity.java
+++ BookShare/app/src/main/java/com/shashanksrikanth/bookshare/MainActivity.java
@@ -1,9 +1,16 @@
 package com.shashanksrikanth.bookshare;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     // First activity that users see after the splash screen- the login page
@@ -12,6 +19,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(!checkNetworkConnection()) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("No network connection!");
+            builder.setMessage("This app requires connectivity to the internet, either via WiFi or data. Please do so before using this app.");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    MainActivity.this.finishAffinity();
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
     }
 
     public void onClickSignIn(View v) {
@@ -26,5 +47,16 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, UserSignIn.class);
         intent.putExtra("signIn", false);
         startActivity(intent);
+    }
+
+    private boolean checkNetworkConnection() {
+        // Check if the device is connected to the internet
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(manager == null) {
+            Toast.makeText(this, "Cannot access ConnectivityManager", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
     }
 }
